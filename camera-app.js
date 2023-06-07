a47855d26c1d79e5165cdd7252ff4e93f73c8022
atkhssn <atk.hssn2017@gmail.com>
2023-06-08 00:59:26 +0600

    get camera permission from the user

--- camera-app.js
+++ camera-app.js
@@ -1,1 +1,14 @@
 console.log('path connected!');
+
+const video = document.getElementById('video');
+
+function startVideo(){
+    navigator.getUserMedia({video: {}}, (stream)=>{
+        video.srcObject = stream;
+        console.log('Permission allow!');
+    }, ()=>{
+        console.log('Permission deny!');
+    })
+}
+
+startVideo();
